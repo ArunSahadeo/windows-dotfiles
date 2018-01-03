4f7c568c80130a04ab8ca5a981b2aab2bc8d1b20
Arun Sahadeo <arun@wirebox.co.uk>
2018-01-03 12:53:10 +0000

    Initial commit

---
+++ aliases.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+DOSKEY htdocs=cd C:\xampp
+DOSKEY Downloads=cd %USERPROFILE%/Downloads
+DOSKEY Applications=cd C:\Program Files
+DOSKEY clear=cls
+DOSKEY ~=cd %USERPROFILE%
+DOSKEY cmder=%USERPROFILE%\Downloads\Cmder.exe
+DOSKEY _vim=cd %USERPROFILE%\.vim
